00e51e00b807827855d80678a0ed0f49168a4a2a
Klime Raykov <47781899+EMWD@users.noreply.github.com>
2020-05-23 17:38:13 +0300

    Update index.php

--- mail_script/index.php
+++ mail_script/index.php
@@ -9,16 +9,16 @@ $email = $_POST['email'];
 $user = R::findOne('users', 'email = ?', array($email));
 
 
-$mail->isSMTP();                					  // Set mailer to use SMTP
+$mail->isSMTP();                	         // Set mailer to use SMTP
 $mail->Host = 'smtp.timeweb.ru';  																							
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'boss@ambassa.ru';				  // email name(FROM)
-$mail->Password = 'your password'; 					  // email password(FROM)
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; 									  // TCP port to connect to / it may be different
+$mail->SMTPAuth = true;                          // Enable SMTP authentication
+$mail->Username = 'boss@ambassa.ru';		 // email name(FROM)
+$mail->Password = 'your password'; 		 // email password(FROM)
+$mail->SMTPSecure = 'ssl';                       // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; 				 // TCP port to connect to / it may be different
 
-$mail->setFrom('boss@ambassa.ru');					  // email name(FROM)
-$mail->addAddress($email);     						  // email(TO) 
+$mail->setFrom('boss@ambassa.ru');		 // email name(FROM)
+$mail->addAddress($email);     			 // email(TO) 
 $mail->isHTML(true);
 
 $mail->Subject = 'message title';
